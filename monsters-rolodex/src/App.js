d45dfc49757d1495c94093b00aed1916ca1ecd77
Stan Jang <dkzks44@gmail.com>
2023-05-17 22:44:07 +0900

    monsters-rolodex: update components

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,6 +1,7 @@
 import { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
 
 class App extends Component {
   constructor() {
@@ -38,19 +39,19 @@ class App extends Component {
     });
     return (
       <div className="App">
-        <input
-          className="search-box"
-          type="search"
-          placeholder="search monsters"
-          onChange={onSearchChange}
-        />
-        {filteredMonsters.map((monster) => {
+        {/* {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
             </div>
           );
-        })}
+        })} */}
+        <SearchBox
+          className="search-box"
+          placeHolder={"input keyword"}
+          onChangeHandler={onSearchChange}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
