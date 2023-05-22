94c12e621829407e73afdb556292b2eda912a9df
Stan Jang <dkzks44@gmail.com>
2023-05-23 02:47:13 +0900

    crwn-clothing: add addToCart feature

--- crwn-clothing/src/components/cart-icon/cart-icon.component.jsx
+++ crwn-clothing/src/components/cart-icon/cart-icon.component.jsx
@@ -6,12 +6,19 @@ import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import "./cart-icon.styles.scss";
 
 const CartIcon = () => {
-  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const { isCartOpen, setIsCartOpen, cartItems, cartCount } =
+    useContext(CartContext);
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+
+  //   const totalCartItemQuantity = cartItems.reduce(
+  //     (acc, val) => acc + val.quantity,
+  //     0
+  //   );
+
   return (
     <div className="cart-icon-container" onClick={toggleIsCartOpen}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{cartCount}</span>
     </div>
   );
 };
