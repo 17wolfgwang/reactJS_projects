d6619c2a6dbe426db0f878d60d5edf60b519e027
Stan Jang <dkzks44@gmail.com>
2023-05-25 18:44:17 +0900

    crwn-clothing: refactoring sass to styled-component

--- crwn-clothing/src/components/cart-icon/cart-icon.component.jsx
+++ crwn-clothing/src/components/cart-icon/cart-icon.component.jsx
@@ -2,13 +2,13 @@ import { useContext } from "react";
 
 import { CartContext } from "../../context/cart.context";
 
-import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
-import "./cart-icon.styles.scss";
+import { CartIconContainer, ShoppingIcon, ItemCount } from "./cart-icon.styles";
 
 const CartIcon = () => {
   const { isCartOpen, setIsCartOpen, cartItems, cartCount } =
     useContext(CartContext);
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+
   // This was my way. But with Context way is more better I think.
   //   const totalCartItemQuantity = cartItems.reduce(
   //     (acc, val) => acc + val.quantity,
@@ -16,10 +16,10 @@ const CartIcon = () => {
   //   );
 
   return (
-    <div className="cart-icon-container" onClick={toggleIsCartOpen}>
-      <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">{cartCount}</span>
-    </div>
+    <CartIconContainer onClick={toggleIsCartOpen}>
+      <ShoppingIcon />
+      <ItemCount>{cartCount}</ItemCount>
+    </CartIconContainer>
   );
 };
 
