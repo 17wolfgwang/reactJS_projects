a514f753b64e1ad90a0e18d772cd48e0c2e626c5
17wolfgwang <dkzks44@gmail.com>
2023-06-08 09:41:31 -0400

    crwn-clothing: update redux toolkit

--- crwn-clothing/src/store/store.js
+++ crwn-clothing/src/store/store.js
@@ -25,6 +25,7 @@ import { rootReducer } from "./root-reducer";
 // };
 // const sagaMiddleware = createSagaMiddleware();
 
+//
 // const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleWares = [
